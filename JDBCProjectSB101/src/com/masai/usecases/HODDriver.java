1795a63baa0ed4c99a713b89ffb64b7682a63e57
Aman Sharma <105920461+thesharmaa@users.noreply.github.com>
2022-10-01 15:35:11 +0530

    Update HODDriver.java

--- JDBCProjectSB101/src/com/masai/usecases/HODDriver.java
+++ JDBCProjectSB101/src/com/masai/usecases/HODDriver.java
@@ -2,7 +2,7 @@ package com.masai.usecases;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
+
 import com.masai.Driver.Driver;
 import com.masai.exception.MyException;
 
@@ -48,35 +48,5 @@ public class HODDriver {
 				}
 			}
 		}
-
-=======
-public class HODDriver {
-
-	public static void main(String[] args) {
-	
-	
-		System.out.println("1. Register Engineer");
-		System.out.println("2. Get details of all Engineers");
-		System.out.println("3. Delete an Engineer");
-		while(true) {
-			System.out.println("Please select an option to continue");
-			
-		Scanner sc=new Scanner(System.in);
-		int choice=sc.nextInt();
-		switch(choice) {
-		case 1:
-		     EngineerRegisterUseCase.main(args);
-		     break;
-		     
-		case 2:
-			GetAllEngineerDetailsUseCase.main(args);
-			break;
-		case 3:
-			DeleteUseCase.main(args);
-			break;
-		}
-		}
-		
->>>>>>> main
 	}
 }
